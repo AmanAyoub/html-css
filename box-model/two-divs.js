fd7ae9d24920b9f33087bc607e1065cd84f66ba2
AmanAyoub <aman.ayoub16@gmail.com>
2025-03-12 12:49:23 +0430

    red + blue

---
+++ box-model/two-divs.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>div</title>
+    <meta charset="utf-8">
+
+    <style>
+      div {
+        width: 300px;
+        height: 300px;
+        box-sizing: border-box;
+        display: inline-block;
+      }
+
+      .first {
+        background-color: #fcc;
+        padding: 20px;
+        margin-right: 20px;
+      }
+
+      .second {
+        border: 5px solid black;
+        background-color: #ccf;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="first">
+
+    </div>
+    <div class="second">
+
+    </div>
+  </body>
+</html>
